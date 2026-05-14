7cafe9e72baca9695fc1676c9f9495e8337ec92c
Anish kumar <anishkumar96084@gmail.com>
2026-05-14 19:03:09 +0530

    ccc

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,8 @@ const registerUser = async (req, res) => {
 
     try {
         const existingUser = await User.findOne({ email });
-        if(existingUser) {
-            return res.status(400).json({ message: "User already exists" });    
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
         }
         // TODO: Hash the password before saving to the data base
         // TODOS: Implement JWT token generation for authentication
@@ -17,18 +17,18 @@ const registerUser = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const user = User.create({name, email, password: hashedPassword});
-        if(user) {
+        const user = User.create({ name, email, password: hashedPassword });
+        if (user) {
             const otp = Math.floor(100000 + Math.random() * 900000).toString();; // Generate a 6-digit OTP
-            
+
             const message = `Your OTP for email verification is: ${otp}`;
             await sendEmail(email, "Email Verification OTP", message);
             return res.status(201).json({
-                
+
             });
         }
-    } catch (error) {
-        return res.status(500).json({ message: "Server error" });
-    }
+    // } catch (error) {
+    //     return res.status(500).json({ message: "Server error" });
+    // }
 };
 
