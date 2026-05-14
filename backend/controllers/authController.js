43983928dc9db2cc1b67e7b23bb3da7558ca9867
Anish kumar <anishkumar96084@gmail.com>
2026-05-14 19:04:05 +0530

    dddd

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,8 +27,8 @@ const registerUser = async (req, res) => {
 
             });
         }
-    // } catch (error) {
-    //     return res.status(500).json({ message: "Server error" });
-    // }
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
 };
 
