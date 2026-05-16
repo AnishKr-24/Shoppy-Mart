68ebb7457ce9fd174bf52481f9f443063c54cc16
Anish kumar <anishkumar96084@gmail.com>
2026-05-16 17:23:53 +0530

    ProductRoutes and ProductControllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const { protect } = require('../middleware/authMiddleware');
 const { admin } = require('../middleware/adminMiddleware');
-
+const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'})
 // All Products...
-router.route('/').get(getProducts).post(protect, admin,  createProduct);
+router.route('/').get(getProducts).post(protect, admin, upload.single('image'),  createProduct);
 //Specific Product
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id').get(getProductById).put(protect, admin, upload.single('image'), updateProduct).delete(protect, admin, deleteProduct);
 
 
 module.exports = router;
