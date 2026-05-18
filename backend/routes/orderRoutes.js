d751424dd6c3a40d5c40584fb14f68759d83d8e4
Anish kumar <anishkumar96084@gmail.com>
2026-05-18 16:58:06 +0530

    kkkkkkk

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,15 +1,12 @@
-const express = required("express");
-const { protect } = require ('../middleware/authMiddleware');
+const express = require("express");
+const { protect } = require('../middleware/authMiddleware');
 const { admin } = require('../middleware/adminMiddleware');
 const { createOrder, getOrders, myOrders, updateOrderStatus } = require("../controllers/orderControllers.js");
 
 const router = express.Router();
 
-
-
-
 router.route('/').post(protect, createOrder).get(protect, admin, getOrders);
-router.route('/myorders').get(protect, getOrderById);
-router.route('/:id').get(protect, myOrders).put(protect,admin,updateOrderStatus);
+router.route('/myorders').get(protect, myOrders);
+router.route('/:id').get(protect, myOrders).put(protect, admin, updateOrderStatus);
 
 module.exports = router;
